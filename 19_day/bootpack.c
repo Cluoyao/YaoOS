d9e1d24585e9b8d32cd977288727b9b700c29881
Y-Luo <luoyao_alpha@outlook.com>
2022-12-31 10:24:21 +0800

    update
    添加换行符打印

--- 19_day/bootpack.c
+++ 19_day/bootpack.c
@@ -689,13 +689,43 @@ type_next_file:
 								/*逐字输出*/
 								s[0] = p[x];
 								s[1] = 0;
-								putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
-								cursor_x += 8;
-								if(cursor_x == 8 + 240)
+								if(s[0] == 0x09)
+								{
+									for(;;)
+									{
+										putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
+										cursor_x += 8;
+										if(cursor_x == 8 + 240)
+										{
+											cursor_x = 8;
+											cursor_y = cons_newline(cursor_y, sheet);
+										}
+										if(((cursor_x - 8) & 0x1f) == 0)
+										{
+											break;
+										}
+									}
+								}
+								else if(s[0] == 0x0a) /* 换行 */
 								{
 									cursor_x = 8;
 									cursor_y = cons_newline(cursor_y, sheet);
 								}
+								else if(s[0] == 0x0d)
+								{
+									/* 暂时不做操作 */
+								}
+								else
+								{
+									putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, s, 1);
+									cursor_x += 8;
+									if(cursor_x == 8 + 240)
+									{
+										cursor_x = 8;
+										cursor_y = cons_newline(cursor_y, sheet);
+									}
+								}
+
 							}
 						}
 						else
