113043495c4a52a4278db076a13d520ae4fc1958
Y-Luo <luoyao_alpha@outlook.com>
2023-01-07 12:26:20 +0800

    create
    创建第20天工程

---
+++ 20_day/bootpack.h
@@ -0,0 +1,305 @@
+/* asmhead.nas */
+struct BOOTINFO { /* 0x0ff0-0x0fff */
+	char cyls; /* 启动区读磁盘读到此为止 */
+	char leds; /* 启动时键盘的LED的状态 */
+	char vmode; /* 显卡模式为多少位彩色 */
+	char reserve;
+	short scrnx, scrny; /* 画面分辨率 */
+	char *vram;
+};
+#define ADR_BOOTINFO	0x00000ff0
+#define ADR_DISKIMG     0x00100000  
+
+/* naskfunc.nas */
+void io_hlt(void);
+void io_cli(void);
+void io_sti(void);
+void io_stihlt(void);
+int io_in8(int port);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+void load_tr(int tr);
+void asm_inthandler20(void);
+void asm_inthandler21(void);
+void asm_inthandler27(void);
+void asm_inthandler2c(void);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
+void farjmp(int eip, int cs);
+
+/* fifo.c */
+
+/* fifo.c */
+typedef struct _FIFO32_ {
+	int           *buf;
+	int            p, q, size, free, flags;
+	struct _TASK_ *task;
+}FIFO32;
+
+void fifo32_init(FIFO32 *fifo, int size, int *buf, struct _TASK_ *task);
+int  fifo32_put(FIFO32 *fifo, int data);
+int  fifo32_get(FIFO32 *fifo);
+int  fifo32_status(FIFO32 *fifo);
+
+/* graphic.c */
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen8(char *vram, int x, int y);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
+
+#define COL8_000000		0    /*  0:黑 */
+#define COL8_FF0000		1    /*  1:梁红 */
+#define COL8_00FF00		2    /*  2:亮绿 */
+#define COL8_FFFF00		3    /*  3:亮黄 */
+#define COL8_0000FF		4    /*  4:亮蓝 */
+#define COL8_FF00FF		5    /*  5:亮紫 */
+#define COL8_00FFFF		6    /*  6:浅亮蓝 */
+#define COL8_FFFFFF		7    /*  7:白 */
+#define COL8_C6C6C6		8    /*  8:亮灰 */
+#define COL8_840000		9    /*  9:暗红 */
+#define COL8_008400		10   /* 10:暗绿 */  
+#define COL8_848400		11   /* 11:暗黄 */
+#define COL8_000084		12   /* 12:暗青 */
+#define COL8_840084		13   /* 13:暗紫 */
+#define COL8_008484		14   /* 14:浅暗蓝 */
+#define COL8_848484		15   /* 15:暗灰 */
+
+/* dsctbl.c */
+struct SEGMENT_DESCRIPTOR {
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+struct GATE_DESCRIPTOR {
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
+};
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+#define ADR_IDT			0x0026f800
+#define LIMIT_IDT		0x000007ff
+#define ADR_GDT			0x00270000
+#define LIMIT_GDT		0x0000ffff
+#define ADR_BOTPAK		0x00280000
+#define LIMIT_BOTPAK	0x0007ffff
+#define AR_DATA32_RW	0x4092
+#define AR_CODE32_ER	0x409a
+#define AR_TSS32		0x0089
+#define AR_INTGATE32	0x008e
+
+/* int.c */
+void init_pic(void);
+void inthandler27(int *esp);
+#define PIC0_ICW1		0x0020
+#define PIC0_OCW2		0x0020
+#define PIC0_IMR		0x0021
+#define PIC0_ICW2		0x0021
+#define PIC0_ICW3		0x0021
+#define PIC0_ICW4		0x0021
+#define PIC1_ICW1		0x00a0
+#define PIC1_OCW2		0x00a0
+#define PIC1_IMR		0x00a1
+#define PIC1_ICW2		0x00a1
+#define PIC1_ICW3		0x00a1
+#define PIC1_ICW4		0x00a1
+
+/* keyboard.c */
+void inthandler21(int *esp);
+void wait_KBC_sendready(void);
+void init_keyboard(FIFO32 *fifo, int data0);
+
+#define PORT_KEYDAT		0x0060
+#define PORT_KEYCMD		0x0064
+
+/* mouse.c */
+struct MOUSE_DEC {
+	unsigned char buf[3], phase;
+	int x, y, btn;
+};
+void inthandler2c(int *esp);
+void enable_mouse(FIFO32 *fifo, int data0, struct MOUSE_DEC *mdec);
+int  mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
+
+
+/* memory.c */
+#define MEMMAN_FREES 4090 /* 大约是32KB(4090 * 8个字节)*/
+#define MEMMAN_ADDR	 0x003c0000
+
+typedef struct _FREEINFO_ 
+{ 	
+	/* 可用信息 */
+	unsigned int addr, size;
+}FREEINFO;
+
+typedef struct _MEMMAN_ 
+{   
+	/* 内存管理 */
+	int frees, maxfrees, lostsize, losts;
+	FREEINFO free[MEMMAN_FREES];
+}MEMMAN;
+
+unsigned int memtest(unsigned int start, unsigned int end);
+void         memman_init(MEMMAN *man);
+unsigned int memman_total(MEMMAN *man);
+unsigned int memman_alloc(MEMMAN *man, unsigned int size);
+int          memman_free(MEMMAN *man, unsigned int addr, unsigned int size);
+unsigned int memman_alloc_4k(MEMMAN *man, unsigned int size);
+int          memman_free_4k(MEMMAN *man, unsigned int addr, unsigned int size);
+
+
+/* 图层信息 */
+#define MAX_SHEETS         256
+#define SHEET_USE          1
+
+typedef struct _SHEET_
+{
+    unsigned char *buf;
+    int            bxsize,   // x方向大小
+                   bysize,   // y方向大小
+                   vx0,      // 左上x
+                   vy0,      // 左上y
+                   col_inv,  // 透明色色号
+                   height,   // 图层高度(代表三维上的高度，比如有三个图层，如果是最上面的图层，那高度就是3（2？）)
+                   flags;    // 图层的设定信息
+	void          *ctl;
+}SHEET;
+
+typedef struct _SHTCTL_
+{
+    unsigned char *vram, *map;  // 整个屏幕的显示首地址,map用于区分不同图层信息
+    int            xsize, // 屏幕分辨率x
+                   ysize, // 屏幕分辨率y
+                   top;   // 最上面图层的高度（代表最上面图层的高度，也表达了图层的个数）
+    SHEET         *sheets[MAX_SHEETS];
+    SHEET          sheets0[MAX_SHEETS];
+}SHTCTL;
+
+SHTCTL* shtctl_init(MEMMAN *memman, unsigned char *vram, int xsize, int ysize);
+SHEET*  sheet_alloc(SHTCTL *ctl);
+void    sheet_setbuf(SHEET *sht, unsigned char *buf, int xsize, int ysize, int col_inv);
+void    sheet_refresh(SHEET *sht, int bx0, int by0, int bx1, int by1);
+void    sheet_slide(SHEET *sht, int vx0, int vy0);
+void    sheet_free(SHEET *sht);
+void    sheet_updown(SHEET *sht, int height);
+void    sheet_refreshsub(SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0, int h1);
+void    sheet_refreshmap(SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0);
+
+
+/* timer.c */
+#define MAX_TIMER 500
+typedef struct _TIMER_
+{
+	/* data */
+	struct _TIMER_ *next;
+	unsigned int    timeout, flags; /* flag 用于记录各个定时器的状态 */
+	FIFO32         *fifo;
+	int             data;
+}TIMER;
+
+typedef struct _TIMERCTL_{
+	unsigned int  count, next; /* next: 下一个超时的时间；*/
+	TIMER         timers0[MAX_TIMER];
+	TIMER        *t0;
+}TIMERCTL;
+
+void   init_pit(void);
+TIMER *timer_alloc(void);
+void   timer_free(TIMER *timer);
+void   timer_init(TIMER *timer, FIFO32 *fifo, int data);
+void   timer_settime(TIMER *timer, unsigned int timeout);
+void   inthandler20(int *esp);
+
+/* multitask */
+#define MAX_TASKS      1000
+#define TASK_GDT0      3
+#define MAX_TASKS_LV   100
+#define MAX_TASKLEVELS 10
+
+typedef struct _TSS32_
+{
+	int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3; /* 任务切换状态 */
+	int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi; /* 32位寄存器 */
+	int es, cs, ss, ds, fs, gs; /* 16位寄存器 */
+	int ldtr, iomap; /* 任务状态记录 */
+}TSS32; /* 104字节 */
+
+extern TIMER *task_timer;
+extern int    mt_tr;
+
+typedef struct _TASK_
+{
+	int     sel, flags;   /* sel用来存放GDT的编号 */
+	int     level, priority;
+	FIFO32  fifo;
+	TSS32   tss;
+}TASK;
+
+typedef struct _TASKLEVEL_
+{
+	int   running; /* 正在运行的任务数量 */
+	int   now;     /* 当前正在运行的是哪个任务 */
+	TASK *tasks[MAX_TASKS_LV];   /* 存放正在运行的任务 */
+}TASKLEVEL;
+
+typedef struct _TASKCTL_
+{
+	int        now_lv; /* 现在活动中的LEVEL*/
+	char       lv_change; /* 在下次任务切换时是否需要改变LEVEL */
+	TASKLEVEL  level[MAX_TASKLEVELS];  
+	TASK       tasks0[MAX_TASKS];
+}TASKCTL;
+
+typedef struct _FILEINFO_
+{
+	unsigned char  name[8], ext[3], type;
+	char           reserve[10];
+	unsigned short time, date, clustno;
+	unsigned int   size;
+}FILEINFO;
+
+
+TASK *task_init(MEMMAN *memman);
+TASK *task_alloc();
+void  task_run(TASK *task, int level, int priority);
+void  task_switch();
+void  task_sleep(TASK *task);
+TASK *task_now();
+void  task_add(TASK *task);
+void  task_switchsub();
+void  task_idle();
+
+/* file.c */
+
+void file_readfat(int *fat, unsigned char *img);
+void file_loadfile(int clustno, int size, char *buf, int *fat, char *img);
+FILEINFO *file_search(char *name, FILEINFO *finfo, int max);
+
+/* window.c */
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
+
+void putfonts8_asc_sht(SHEET *sht, int x, int y, int c, int b, char *s, int l);
+
+void make_textbox8(SHEET *sht, int x0, int y0, int sx, int sy, int c);
+
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
+
+/* console.c */
+void console_task(SHEET *sheet, unsigned int memtotal);
+void cons_newline(CONSOLE *cons);
+void cons_putchar(CONSOLE *cons, int chr, char move);
+void cons_runcmd(char *cmdline, CONSOLE *cons, int *fat, unsigned int memtotal);
+void cmd_mem(CONSOLE *cons, unsigned int memtotal);
+void display_prompt(CONSOLE *cons, char *str, char move);
+void cmd_clear(CONSOLE *cons);
+void cmd_ly(CONSOLE *cons);
+void cmd_ls(CONSOLE *cons);
+void cmd_type(CONSOLE *cons, int *fat, char *cmdline);
+void cmd_hlt(CONSOLE *cons, int *fat);
---
+++ 20_day/console.c
@@ -0,0 +1,380 @@
+#include "bootpack.h"
+#include <stdio.h>
+#include <string.h>
+
+#define CURSOR_INIT_POS  64
+
+typedef struct _CONSOLE_{
+	SHEET *sht;
+	int    cur_x, cur_y, cur_c;
+}CONSOLE;
+
+void cons_putchar(CONSOLE *cons, int chr, char move)
+{
+	char s[2];
+	s[0] = chr;
+	s[1] = 0;
+
+	if(s[0] == 0x09) /* 制表符 */
+	{
+		for(;;)
+		{
+			putfonts8_asc_sht(cons->sht, cons->cur_x, cons->cur_y, COL8_FFFFFF, COL8_000000, " ", 1);
+			cons->cur_x += 8;
+			if(cons->cur_x == 8 + 240)
+			{
+				cons->cur_x = 8;
+				cons_newline(cons);
+			}
+			if(((cons->cur_x - 8) & 0x1f) == 0)
+			{
+				break;
+			}
+		}
+	}
+	else if(s[0] == 0x0a) /* 换行 */
+	{
+		cons_newline(cons);
+	}
+	else if(s[0] == 0x0d)
+	{
+		/* 暂时不做操作 */
+	}
+	else
+	{
+		putfonts8_asc_sht(cons->sht, cons->cur_x, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 1);
+		if(move != 0)
+		{
+			/* move为0时光标不后移 */
+			cons->cur_x += 8;
+			if(cons->cur_x == 8 + 240)
+			{
+				cons_newline(cons);
+			}
+		}
+	}
+	return;
+}
+
+void display_prompt(CONSOLE *cons, char *str, char move)
+{
+	int len = strlen(str);
+	int i;
+	for(i = 0; i < len; i++)
+	{
+		cons_putchar(&cons, str[i], move);
+	}
+	return;
+}
+
+void console_task(SHEET *sheet, unsigned int memtotal)
+{
+	TIMER                     *timer;
+	TASK                      *task            = task_now();
+	MEMMAN                    *memman          = (MEMMAN *)MEMMAN_ADDR;
+	int                        cursor_init_pos = CURSOR_INIT_POS;
+	int                        i, fifobuf[128];
+	char                       s[30];
+	int                        x,y;
+	char                       cmdline[30], *p;
+	CONSOLE                    cons;
+	FILEINFO                  *finfo  = (FILEINFO *)(ADR_DISKIMG + 0x002600);
+	int                       *fat    = (int *)memman_alloc_4k(memman, 4 * 2880);
+    struct SEGMENT_DESCRIPTOR *gdt    = (struct SEGMENT_DESCRIPTOR *)ADR_GDT;
+	cons.sht                          = sheet;
+	cons.cur_x                        = cursor_init_pos;
+	cons.cur_y                        = 28;
+	cons.cur_c                        = -1;
+
+	fifo32_init(&task->fifo, 128, fifobuf, task);
+	timer = timer_alloc();
+	timer_init(timer, &task->fifo, 1);
+	timer_settime(timer, 50);
+	file_readfat(fat, (unsigned char*)(ADR_DISKIMG + 0x000200));
+
+	/* 显示提示符 */
+	//putfonts8_asc_sht(sheet, 8, 29, COL8_FFFFFF, COL8_000000, "YaoOs>", 6);
+    display_prompt(&cons, "YaoOs>", 1);
+
+	for(;;)
+	{
+		io_cli();
+		if(fifo32_status(&task->fifo) == 0)
+		{
+			task_sleep(task);
+			io_sti();
+		}
+		else
+		{
+			i = fifo32_get(&task->fifo);
+			io_sti();
+			if(i <= 1)  /* 光标用定时器 */
+			{
+				if(i != 0)
+				{
+					timer_init(timer, &task->fifo, 0); /* 下次置0 */
+					if(cons.cur_c >= 0) /* 光标其实还是在闪，只是没有颜色 */
+					{
+						cons.cur_c = COL8_FFFFFF;
+					}
+
+				}
+				else
+				{
+					timer_init(timer, &task->fifo, 1);
+					if(cons.cur_c >= 0) /* 光标其实还是在闪，只是没有颜色 */
+					{
+						cons.cur_c = COL8_000000;
+					}
+
+				}
+				timer_settime(timer, 50);
+			}
+			if(i == 2)
+			{
+				cons.cur_c = COL8_FFFFFF; 
+			}
+			if(i == 3) /* 光标off */
+			{
+				/* 因为背景是黑色，所以用黑色擦除 */
+				boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15); /* 擦除 */
+				cons.cur_c = -1;
+			}
+			if(i >= 256 && i <= 511)
+		    {
+				if(i == 8 + 256)
+				{
+					/* 控制退格键移动*/
+					if(cons.cur_x > cursor_init_pos)
+					{
+						/* 用空白擦除光标后将光标前移一位 */
+						cons_putchar(&cons, ' ', 0);
+						cons.cur_x -= 8;
+					}
+				}
+				else if(i == 10 + 256)
+				{
+					/* 回车键 */
+					/* 用空格将光标擦除 */
+					cons_putchar(&cons, ' ', 0);
+					cmdline[cons.cur_x / 8 - cursor_init_pos / 8] = 0;
+					cons_newline(&cons);
+					/* 执行命令 */
+					cons_runcmd(cmdline, &cons, fat, memtotal);
+					/* 执行完之后显示提示符 */
+					display_prompt(&cons, "YaoOs>", 1);
+				}
+				else
+				{
+					if(cons.cur_x < 240)
+					{
+						/* 显示一个字符之后将光标后移一位 */
+						cmdline[cons.cur_x / 8 - cursor_init_pos / 8] = i - 256;
+						cons_putchar(&cons, i - 256, 1);
+					}
+				}
+			}
+			/* 重新显示光标 */	
+			if(cons.cur_x >= 0)
+			{
+				boxfill8(sheet->buf, sheet->bxsize, cons.cur_c, cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15);
+			}			
+			sheet_refresh(sheet, cons.cur_x, cons.cur_y, cons.cur_x + 8, cons.cur_y + 16);
+		}
+	}
+}
+
+
+void  cons_newline(CONSOLE *cons)
+{
+	int    x, y;
+	SHEET *sheet = cons->sht;
+	if(cons->cur_y < 28 + 112)
+	{
+		cons->cur_y += 16;
+	}
+	else
+	{
+		/* 滚动 */
+		for(y = 28; y < 28 + 112; y++)
+		{
+			for(x = 8; x < 8 + 240; x++)
+			{
+				sheet->buf[x + y * sheet->bxsize] = sheet->buf[x + (y + 16) * sheet->bxsize];
+			}
+		}
+		for(y = 28 + 112; y < 28 + 128; y++)
+		{
+			for(x = 8; x < 8 + 240; x++)
+			{
+				sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+			}
+		}
+		sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
+	}
+	cons->cur_x = 8;
+	return;
+}
+
+void cons_runcmd(char *cmdline, CONSOLE *cons, int *fat, unsigned int memtotal)
+{
+	if(strcmp(cmdline, "mem") == 0)
+	{
+		cmd_mem(cons, memtotal);
+	}
+	else if(strcmp(cmdline, "clear") == 0)
+	{
+		cmd_clear(cons);
+	}
+	else if(strcmp(cmdline, "luoyao") == 0)
+	{
+		cmd_ly(cons);
+	}
+	else if(strcmp(cmdline, "ls") == 0)
+	{	
+		cmd_ls(cons);
+	}
+	else if(strncmp(cmdline, "type ", 5) == 0)
+	{
+		cmd_type(cons, fat, cmdline);
+	}
+	else if(strcmp(cmdline, "hlt") == 0)
+	{
+		cmd_hlt(cons, fat);
+	}
+	else if(cmdline[0] != 0)
+	{
+		putfonts8_asc_sht(cons->sht, CURSOR_INIT_POS, cons->cur_y, COL8_FFFFFF, COL8_000000, "Bad command", 12);
+		cons_newline(cons);
+		cons_newline(cons);
+	}
+	return ;
+}
+
+void cmd_mem(CONSOLE *cons, unsigned int memtotal)
+{
+	MEMMAN                    *memman          = (MEMMAN *)MEMMAN_ADDR;
+	char                       s[30];
+	/* */
+	sprintf(s, "total memory %dMB", memtotal / (1024 * 1024));
+	/* 在背景图层上显示信息 */
+	putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 30);
+	cons_newline(cons);
+	sprintf(s, "free : %dKB", memman_total(memman) / 1024);
+	/* 在背景图层上显示信息 */
+	putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 30);
+	cons_newline(cons);
+	cons_newline(cons);
+	return;
+}
+
+void cmd_clear(CONSOLE *cons)
+{
+	int x, y;
+	SHEET *sheet = cons->sht;
+	/* 清除界面的命令 */
+	for(y = 28; y < 28 + 128; y++)
+	{
+		for(x = 8; x < 8 + 240; x++)
+		{
+			sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+		}
+	}
+	sheet_refresh(cons->sht, 8, 28, 8 + 240, 28 + 128);
+	cons->cur_x = 28;
+	return ;
+}
+
+void cmd_ly(CONSOLE *cons)
+{
+	char s[30];
+	sprintf(s, "%s", "LuoY is a handsome man.");
+	/* 在背景图层上显示信息 */
+	putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 24);
+	cons_newline(cons);
+	return;
+}
+
+void cmd_ls(CONSOLE *cons)
+{
+	FILEINFO    *finfo  = (FILEINFO *)(ADR_DISKIMG + 0x002600);
+	int          i, j;
+	char         s[30];
+
+	/* ls命令 */
+	for(i = 0; i < 224; i++)
+	{
+		if(finfo[i].name[0] == 0x00)
+		{
+			break; /* 不包含任何文件信息 */
+		}
+		if(finfo[i].name[0] != 0xe5)
+		{
+			if((finfo[i].type & 0x18) == 0)
+			{
+				sprintf(s, "filename.ext %7d", finfo[i].size);
+				for(j = 0; j < 8; j++)
+				{
+					s[j] = finfo[i].name[j];
+				}
+				s[9]  = finfo[i].ext[0];
+				s[10] = finfo[i].ext[1];
+				s[11] = finfo[i].ext[2];
+				/* 在背景图层上显示信息 */
+				putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, s, 30);
+				cons_newline(cons);
+			}
+		}
+	}
+	cons_newline(cons);
+	return;
+}
+
+void cmd_type(CONSOLE *cons, int *fat, char *cmdline)
+{
+	MEMMAN   *memman          = (MEMMAN *)MEMMAN_ADDR;
+	FILEINFO *finfo = file_search(cmdline + 5, (FILEINFO *)(ADR_DISKIMG + 0x002600), 224);
+	char     *p;
+	int       i;
+	if(finfo != 0)
+	{
+		p = (char *)memman_alloc_4k(memman, finfo->size);
+		file_loadfile(finfo->clustno, finfo->size, p, fat, (char *)(ADR_DISKIMG + 0x003e00));
+		for(i = 0; i < finfo->size; i++)
+		{
+			cons_putchar(cons, p[i], 1);
+		}
+		memman_free_4k(memman, (int)p, finfo->size);
+	}
+	else
+	{
+		putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "file not found.", 15);
+		cons_newline(cons);
+	}
+	return;
+}
+
+void cmd_hlt(CONSOLE *cons, int *fat)
+{
+	MEMMAN                    *memman = (MEMMAN *)MEMMAN_ADDR;
+	FILEINFO                  *finfo  = file_search("HLT.HRB", (FILEINFO *)(ADR_DISKIMG + 0x002600), 224);
+	struct SEGMENT_DESCRIPTOR *gdt    = (struct SEGMENT_DESCRIPTOR *)ADR_GDT;
+	char                      *p;
+
+	if(finfo != 0)
+	{
+		/* 分配读入的存储空间 */
+		p = (char *)memman_alloc_4k(memman, finfo->size);
+		file_loadfile(finfo->clustno, finfo->size, p, fat, (char *)(ADR_DISKIMG + 0x003e00));
+		set_segmdesc(gdt + 1003, finfo->size - 1, (int)p, AR_CODE32_ER);
+		farjmp(0, 1003 * 8);
+		memman_free_4k(memman, (int)p, finfo->size);
+	}
+	else
+	{
+		putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_FFFFFF, COL8_000000, "file not found.", 15);
+		cons_newline(cons);
+	}
+	cons_newline(cons);
+	return;
+}
