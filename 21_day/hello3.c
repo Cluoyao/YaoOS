987590b9a94bc0e7adfb4bb7c206416335efa85e
Y-Luo <luoyao_alpha@outlook.com>
2023-01-08 12:26:13 +0800

    add hello3
    添加c语言的调用代码

---
+++ 21_day/hello3.c
@@ -0,0 +1,11 @@
+void api_putchar(int c);
+
+void HariMain(void)
+{
+    api_putchar('h');
+    api_putchar('i');
+    api_putchar('h');
+    api_putchar('a');
+    api_putchar('o');
+    return;
+}
