3a5a5bc75b5ff3dd656d4e0d76654c21d8d27053
Y-Luo <luoyao_alpha@outlook.com>
2023-02-11 16:08:55 +0800

    close windows
    关闭窗口

--- 23_day/console.c
+++ 23_day/console.c
@@ -593,6 +593,10 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 			sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
 		}
 	}
+	else if(edx == 14)
+	{
+		sheet_free((SHEET *)ebx);
+	}
 	return 0;
 }
 
