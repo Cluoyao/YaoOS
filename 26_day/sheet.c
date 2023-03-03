3801f104dc007e0a846f43380b5117d1cbaecdfe
Y-Luo <luoyao_alpha@outlook.com>
2023-03-03 16:44:29 +0800

    解决问题
    之前应该是哪里的代码没有写对，导致画面异常，这边半天又回退不回去，重新提交了内容

--- 26_day/sheet.c
+++ 26_day/sheet.c
@@ -141,7 +141,7 @@ void sheet_refreshmap(SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0)
 /* vx0,vy0:移动前的位置；vx1,vy1:移动后的位置 */
 void sheet_refreshsub(SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0, int h1)
 {
-    int            h, bx, by, vx, vy, bx0, by0, bx1, by1;
+	int h, bx, by, vx, vy, bx0, by0, bx1, by1, bx2, sid4, i, i1, *p, *q, *r;
     unsigned char *buf, c, *vram = ctl->vram, *map = ctl->map, sid;
     SHEET         *sht;
 
@@ -157,35 +157,80 @@ void sheet_refreshsub(SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0, i
 
         sid = sht - ctl->sheets0;
 
-        /* 使用vx0~vy1,对bx0~by1进行倒推 */
-        bx0 = vx0 - sht->vx0;
-        by0 = vy0 - sht->vy0;
-        bx1 = vx1 - sht->vx0;
-        by1 = vy1 - sht->vy0;
-        if(bx0 < 0){bx0 = 0;}
-        if(by0 < 0){by0 = 0;}
-        if(bx1 > sht->bxsize){bx1 = sht->bxsize;}
-        if(by1 > sht->bysize){by1 = sht->bysize;}
-
-        for(by = by0; by < by1; by++)
-        {
-            vy = sht->vy0 + by;
-            for(bx = bx0; bx < bx1; bx++)
-            {
-                vx = sht->vx0 + bx;
-
-                /* 如果属于当前图层（sid），我才进行绘制 */
-                if(map[vy * ctl->xsize + vx] == sid)
-                {
-                    /* 这是访问显存信息的方式 */
-                    vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx]; /* buf存放的是背景的颜色信息 */
-                }
-
-            }
-        }
-    }
-
-    return;
+		/* 使用vx0～vy1，对bx0～by1进行倒推 */
+		bx0 = vx0 - sht->vx0;
+		by0 = vy0 - sht->vy0;
+		bx1 = vx1 - sht->vx0;
+		by1 = vy1 - sht->vy0;
+		if (bx0 < 0) { bx0 = 0; } /* 处理刷新范围在图层外侧 */
+		if (by0 < 0) { by0 = 0; }
+		if (bx1 > sht->bxsize) { bx1 = sht->bxsize; } /* 应对不同的重叠方式 */
+		if (by1 > sht->bysize) { by1 = sht->bysize; }
+		if ((sht->vx0 & 3) == 0) {
+			/* 4字节型*/
+			i = (bx0 + 3) / 4; /* bx0除以4（小数进位）*/
+			i1 = bx1 / 4;      /* bx1除以4（小数舍去）*/
+			i1 = i1 - i;
+			sid4 = sid | sid << 8 | sid << 16 | sid << 24;
+			for (by = by0; by < by1; by++) {
+				vy = sht->vy0 + by;
+				for (bx = bx0; bx < bx1 && (bx & 3) != 0; bx++) { 
+					/*前面被4除多余的部分逐个字节写入*/
+					vx = sht->vx0 + bx;
+					if (map[vy * ctl->xsize + vx] == sid)
+					 {
+						vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+					}
+				}
+				vx = sht->vx0 + bx;
+				p = (int *) &map[vy * ctl->xsize + vx];
+				q = (int *) &vram[vy * ctl->xsize + vx];
+				r = (int *) &buf[by * sht->bxsize + bx];
+				for (i = 0; i < i1; i++) 
+				{ 
+					/* 4的倍数部分*/
+					if (p[i] == sid4) 
+					{
+						q[i] = r[i]; /*估计大多数会是这种情况，因此速度会变快*/
+					} else {
+						bx2 = bx + i * 4;
+						vx = sht->vx0 + bx2;
+						if (map[vy * ctl->xsize + vx + 0] == sid) {
+							vram[vy * ctl->xsize + vx + 0] = buf[by * sht->bxsize + bx2 + 0];
+						}
+						if (map[vy * ctl->xsize + vx + 1] == sid) {
+							vram[vy * ctl->xsize + vx + 1] = buf[by * sht->bxsize + bx2 + 1];
+						}
+						if (map[vy * ctl->xsize + vx + 2] == sid) {
+							vram[vy * ctl->xsize + vx + 2] = buf[by * sht->bxsize + bx2 + 2];
+						}
+						if (map[vy * ctl->xsize + vx + 3] == sid) {
+							vram[vy * ctl->xsize + vx + 3] = buf[by * sht->bxsize + bx2 + 3];
+						}
+					}
+				}
+				for (bx += i1 * 4; bx < bx1; bx++) { 
+					/*后面被4除多余的部分逐个字节写入*/
+					vx = sht->vx0 + bx;
+					if (map[vy * ctl->xsize + vx] == sid) {
+						vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+					}
+				}
+			}
+		} else {
+			/* 1字节型*/
+			for (by = by0; by < by1; by++) {
+				vy = sht->vy0 + by;
+				for (bx = bx0; bx < bx1; bx++) {
+					vx = sht->vx0 + bx;
+					if (map[vy * ctl->xsize + vx] == sid) {
+						vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+					}
+				}
+			}
+		}
+	}
+	return;
 }
 
 
