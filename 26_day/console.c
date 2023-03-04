0dbdb9f40ef59b1a12996b100b597760a62cf0a4
Y-Luo <luoyao_alpha@outlook.com>
2023-03-04 13:46:38 +0800

    "x" function
    实现鼠标点击关闭的功能

--- 26_day/console.c
+++ 26_day/console.c
@@ -137,6 +137,11 @@ void console_task(SHEET *sheet, unsigned int memtotal)
 				boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15); /* 擦除 */
 				cons.cur_c = -1;
 			}
+			if(i == 4)
+			{
+				/*点击命令行窗口的"x"按钮"*/
+				cmd_exit(&cons, fat);
+			}
 			if(i >= 256 && i <= 511)
 		    {
 				if(i == 8 + 256)
